a1c48463ddedabf2ac3093655a9743406b5a415c
Alessandro Menezes <alessandroadm@live.com>
2020-06-04 17:20:52 -0300

    criando criptografia

---
+++ src/js/createCredentials.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('btn-generate');
+const btnDecode = document.getElementById('btn-decode');
+const inputCredential = document.getElementById('credential');
+const inputCredentialDecode = document.getElementById('credential2');
+const userName = document.getElementById('name');
+const password = document.getElementById('password');
+const code = document.getElementById('code');
+const decoded = document.getElementById('decoded');
+
+function generateCredential() {
+  
+  let user = { email: userName.value, password: password.value };
+  let credential = CryptoJS.AES.encrypt(JSON.stringify(user), 'credential').toString();
+  inputCredential.value = credential;
+  code.innerText = `headers:{credential:'${credential}'}`;
+}
+
+function decodeCredential() {
+  let decode = CryptoJS.AES.decrypt(inputCredentialDecode.value, "credential");
+  let credential = JSON.parse(decode.toString(CryptoJS.enc.Utf8));
+  decoded.innerText = `${JSON.stringify(credential)}`;
+}
+
+btnDecode.addEventListener('click', decodeCredential);
+btn.addEventListener('click', generateCredential);
