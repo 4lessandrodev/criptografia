a23b1118d32a61564e836fcd763d7d5cfe741254
Alessandro dev <34426407+4lessandrodev@users.noreply.github.com>
2021-01-16 01:12:05 -0300

    Update createCredentials.js

--- src/js/createCredentials.js
+++ src/js/createCredentials.js
@@ -6,17 +6,18 @@ const userName = document.getElementById('name');
 const password = document.getElementById('password');
 const code = document.getElementById('code');
 const decoded = document.getElementById('decoded');
+var secret = "credential"
 
 function generateCredential() {
   
   let user = { email: userName.value, password: password.value };
-  let credential = CryptoJS.AES.encrypt(JSON.stringify(user), 'credential').toString();
+  let credential = CryptoJS.AES.encrypt(JSON.stringify(user), secret).toString();
   inputCredential.value = credential;
   code.innerText = `headers:{credential:'${credential}'}`;
 }
 
 function decodeCredential() {
-  let decode = CryptoJS.AES.decrypt(inputCredentialDecode.value, "credential");
+  let decode = CryptoJS.AES.decrypt(inputCredentialDecode.value, secret);
   let credential = JSON.parse(decode.toString(CryptoJS.enc.Utf8));
   decoded.innerText = `${JSON.stringify(credential)}`;
 }
